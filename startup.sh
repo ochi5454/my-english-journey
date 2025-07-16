00336cfeaf388aa970c5951bd0874da1ca4ab358
emitbreaker-taiwan <emitbreaker.taiwan@gmail.com>
2025-07-16 16:12:48 +0900

    1

--- startup.sh
+++ startup.sh
@@ -5,11 +5,11 @@ set -e
 export PYTHONPATH=$PYTHONPATH:./backend
 
 # 2. （ビルド済みフロントを配信したい場合は）npm run build などをここで呼ぶ
-# npm install && npm run build
+npm install && npm run build
 
 # 3. Oryx ビルド済みの仮想環境を使う場合、pip 再インストールは不要
 #    もし必要なら以下を有効化
-# python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # ここではnpm install & buildは実行しない（ローカルでビルド済みファイルを配置済みと仮定）
 # もしApp Service上でNode.jsのビルドも強制的に行いたい場合、
