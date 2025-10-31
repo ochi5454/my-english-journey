68615e088196e3a7c1d508b468f28b05b9d8aac2
Junhyeok CHoi <emitbreaker-taiwan@gmail.com>
2025-10-31 09:23:12 +0900

    Commit

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://prothentia.ngrok.app/"],  # フロントエンドのURL
+    allow_origins=["http://localhost:5173", "https://prothentia.ngrok.app"],  # フロントエンドのURL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: "https://prothentia.ngrok.app" // ← ここを本番用に書き換えればOK
+  API_BASE_URL: "https://prothentia.ngrok.dev" // ← ここを本番用に書き換えればOK
 };
 
 export default config;
